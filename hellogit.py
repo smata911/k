a29486b2e9f18278501c46fff6d3494dbcc5ddb7
sergiomata911@gmail.com <sergiomata911@gmail.com>
2024-07-16 11:36:35 -0600

    hello desde github

--- hellogit.py
+++ hellogit.py
@@ -1,3 +1,3 @@
-print("hola git")
-print("que tal bro")
-print("que tal bro2222222222222222222223333")
+print("hola github")
+print("que tal bro desde github")
+print("que tal bro2222222222222222222223333 desde github")
